314e7f0c1aa297d21171e70cd53161fff0838497
Daniel Sasse <dsasse07@gmail.com>
2021-02-04 09:56:01 -0500

    Deliverable 1 Done

--- index.js
+++ index.js
@@ -3,4 +3,49 @@ document.addEventListener("DOMContentLoaded", function() {
   init();
 
   // ADD CODE HERE!
-});
+  // Build Li
+  // Fill textCOntent
+  // Append to ul
+
+  const ul = document.querySelector('#moves-container')
+  let steps = []
+  function buildLi (direction) {
+    let li = document.createElement("li")
+    steps.push(li)
+    li.dataset.step = steps.indexOf(li) + 1
+    li.textContent = direction
+
+    ul.append(li)
+  }
+
+
+
+  window.addEventListener('keydown', e => {
+    
+    switch (e.keyCode) { 
+      case 37: 
+          buildLi('left')
+          break; 
+      case 38: 
+          buildLi('up')
+          break; 
+      case 39: 
+          buildLi('right')
+          break; 
+      case 40: 
+          buildLi('down')
+          break; 
+    } 
+  })
+
+
+
+
+
+
+
+
+
+
+
+})
