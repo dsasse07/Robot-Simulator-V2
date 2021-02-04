93b2aafecf83ba3cf88f4a5b51bb896bfdab2bd2
Alex Marz <alexhmarz@gmail.com>
2021-02-04 11:00:33 -0500

    stuck on interval

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ document.addEventListener("DOMContentLoaded", function() {
   // Build Li
   // Fill textCOntent
   // Append to ul
+  const control = document.querySelector("#control-panel")
+
 
   const ul = document.querySelector('#moves-container')
   let steps = []
@@ -14,10 +16,40 @@ document.addEventListener("DOMContentLoaded", function() {
     steps.push(li)
     li.dataset.step = steps.indexOf(li) + 1
     li.textContent = direction
+    li.className = "step"
 
     ul.append(li)
   }
 
+  const activateRobot = _ => {
+ let totalSteps = steps.length
+ let count = 0
+  setInterval(moveRobot(totalSteps), 500)
+ 
+function moveRobot(totalSteps){
+  console.log(totalSteps)
+  console.log(count)
+  if (count == totalSteps) clearInterval()
+  else {
+  let currentLi = steps.shift()
+  move(currentLi.textContent)
+  currentLi.remove()
+    count++;
+}
+
+}
+  }
+// function moveRobot(totalSteps, t){
+//   console.log(totalSteps)
+//   console.log(t)
+//   if (t == totalSteps) clearInterval(t)
+//   else {
+//   let currentLi = steps.shift()
+//   move(currentLi.textContent)
+//   currentLi.remove()}
+
+  
+//}
 
 
   window.addEventListener('keydown', e => {
@@ -35,8 +67,24 @@ document.addEventListener("DOMContentLoaded", function() {
       case 40: 
           buildLi('down')
           break; 
-    } 
+    }
+
+
+
   })
+
+
+  control.addEventListener('click', e => {
+    if (e.target.id === "move-button"){
+      activateRobot()
+    } else if (e.target.className === "step") {
+      let index = steps.indexOf(e.target)
+      steps.splice(index, 1)
+      e.target.remove()
+    }
+
+  })
+
 
 
 
